e1eb78d6f473a0f6cc28e12081256e64a10e0353
CaolanCode <cmacmahon3@gmail.com>
2023-02-07 06:27:03 +0000

    commit exercise 1

---
+++ day-05/app.js
@@ -0,0 +1,41 @@
+// Exercise Level 1
+const empty = new Array()
+const six = [1,2,3,4,5,6,7]
+console.log(six.length)
+const middle = Math.floor(six.length/2)
+console.log(six[0], six[middle], six[six.length-1])
+const mixedDataTypes = [1, 'Bob', false, 'Monday', 3.5, true]
+console.log(mixedDataTypes.length)
+let itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+console.log(itCompanies)
+console.log(itCompanies.length)
+const middleCompany = Math.floor(itCompanies.length/2)
+console.log(itCompanies[0], itCompanies[middleCompany], itCompanies[itCompanies.length-1])
+itCompanies.forEach(company => console.log(company))
+itCompanies = itCompanies.map(company => company.toUpperCase())
+console.log(itCompanies)
+itCompanies = itCompanies.map(company => company.toLowerCase())
+console.log(itCompanies.toString() + ' are big IT companies')
+if(itCompanies.includes('AMAZON')) {
+    console.log('Amazon exists')
+} else {
+    console.log('Amazon doesn\'t exists')
+}
+itCompanies = itCompanies.filter(company => !company.includes('oo'))
+console.log(itCompanies)
+itCompanies.sort()
+console.log(itCompanies)
+itCompanies.reverse()
+console.log(itCompanies)
+console.log(itCompanies.slice(0,3))
+console.log(itCompanies.slice(1,-1))
+itCompanies.shift()
+console.log(itCompanies)
+itCompanies.shift(itCompanies.length/2)
+console.log(itCompanies)
+itCompanies.splice(itCompanies.length/2, 1)
+console.log(itCompanies)
+itCompanies.pop()
+console.log(itCompanies)
+itCompanies = []
+console.log(itCompanies)
