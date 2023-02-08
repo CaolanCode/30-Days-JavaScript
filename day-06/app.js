6d5e8a709ac0928f544fbd2ac0a00a8146b0ad12
CaolanCode <cmacmahon3@gmail.com>
2023-02-08 07:09:50 +0000

    commit day 6

--- day-06/app.js
+++ day-06/app.js
@@ -132,3 +132,89 @@ console.log(capCountries)
 
 const countryLength = countries.map(country => country.length)
 console.log(countryLength)
+
+const listOfCountries = []
+countries.forEach(country => {
+    listOfCountries.push([country,country.substring(0,3).toUpperCase(), country.length])
+})
+console.log(listOfCountries)
+
+const landArray = countries.filter(country => country.includes('land'))
+if(landArray.length > 0) {
+    console.log(landArray)
+} else {
+    console.log('All these countries without land')
+}
+
+const iaArray = countries.filter(country => country.substring(country.length-2) === 'ia')
+if(iaArray.length > 0) {
+    console.log(iaArray)
+} else {
+    console.log('These are countries that end without ia')
+}
+
+let bigWord = ""
+countries.forEach(country => {
+    if(country.length > bigWord.length) {
+        bigWord = country
+    }
+})
+console.log(bigWord)
+
+const fiveChars = countries.filter(country => country.length === 5)
+console.log(fiveChars)
+
+let largestWeb = webTechs[0]
+for(let i = 1; i < webTechs.length; i++) {
+    if(webTechs[i].length > largestWeb.length) {
+        largestWeb = webTechs[i]
+    }
+}
+console.log(largestWeb)
+
+const webSize = webTechs.map(item => [item, item.length])
+console.log(webSize)
+
+const mernArr = ['MongoDB', 'Express', 'React', 'Node']
+let mern = mernArr.map(item => item.charAt(0))
+mern = mern.join('')
+console.log(mern)
+
+webTechs.forEach(item => console.log(item))
+
+let fruit = ['banana', 'orange', 'mango', 'lemon']
+for(let i = 0; i < fruit.length/2; i++) {
+    let temp = fruit[fruit.length - 1 - i]
+    fruit[fruit.length - 1 - i] = fruit[i]
+    fruit[i] = temp
+}
+console.log(fruit)
+
+const fullStack = [
+    ['HTML', 'CSS', 'JS', 'React'],
+    ['Node', 'Express', 'MongoDB']
+  ]
+fullStack.forEach(item => {
+    item.forEach(i => console.log(i))
+})
+
+// exercise 3
+let copyCountries = []
+countries.forEach(country => copyCountries.push(country))
+console.log(copyCountries)
+
+const sortedCountries = copyCountries.sort((a,b) => a > b ? 1 : -1)
+console.log(sortedCountries)
+
+const sortedWebTech = webTechs.sort((a,b) => a > b ? 1 : -1)
+console.log(sortedWebTech)
+
+const sortedMern = mernArr.sort((a,b) => a > b ? 1 : -1)
+console.log(sortedMern)
+
+const twoWords = countries.filter(country => country.includes(' '))
+if(twoWords.length > 0) {
+    console.log(twoWords)
+} else {
+    console.log("No country has two words")
+}
