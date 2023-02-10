4675638ee04c40e9543b3017af693d8c8dfaae4c
CaolanCode <cmacmahon3@gmail.com>
2023-02-10 16:32:47 +0000

    commit exercise 3

--- day-08/app.js
+++ day-08/app.js
@@ -103,4 +103,118 @@ users.Bob.points = 40
 const keys = Object.keys(users)
 console.log(keys)
 
-//
+// Exercise 3
+const users2 = [
+  {
+      _id: 'ab12ex',
+      username: 'Alex',
+      email: 'alex@alex.com',
+      password: '123123',
+      createdAt:'08/01/2020 9:00 AM',
+      isLoggedIn: false
+  },
+  {
+      _id: 'fg12cy',
+      username: 'Asab',
+      email: 'asab@asab.com',
+      password: '123456',
+      createdAt:'08/01/2020 9:30 AM',
+      isLoggedIn: true
+  },
+  {
+      _id: 'zwf8md',
+      username: 'Brook',
+      email: 'brook@brook.com',
+      password: '123111',
+      createdAt:'08/01/2020 9:45 AM',
+      isLoggedIn: true
+  },
+  {
+      _id: 'eefamr',
+      username: 'Martha',
+      email: 'martha@martha.com',
+      password: '123222',
+      createdAt:'08/01/2020 9:50 AM',
+      isLoggedIn: false
+  },
+  {
+      _id: 'ghderc',
+      username: 'Thomas',
+      email: 'thomas@thomas.com',
+      password: '123333',
+      createdAt:'08/01/2020 10:00 AM',
+      isLoggedIn: false
+  }
+  ];
+
+  const products = [
+{
+  _id: 'eedfcf',
+  name: 'mobile phone',
+  description: 'Huawei Honor',
+  price: 200,
+  ratings: [
+    { userId: 'fg12cy', rate: 5 },
+    { userId: 'zwf8md', rate: 4.5 }
+  ],
+  likes: []
+},
+{
+  _id: 'aegfal',
+  name: 'Laptop',
+  description: 'MacPro: System Darwin',
+  price: 2500,
+  ratings: [],
+  likes: ['fg12cy']
+},
+{
+  _id: 'hedfcg',
+  name: 'TV',
+  description: 'Smart TV:Procaster',
+  price: 400,
+  ratings: [{ userId: 'fg12cy', rate: 5 }],
+  likes: ['fg12cy']
+}
+]
+function signIn() {
+  let loggedIn = false
+  const username = prompt('Enter username')
+  const password = prompt('Enter password')
+  for(const user in users2) {
+    if(users2[user].username === username && users2[user].password === password) {
+      loggedIn = true
+    }
+  }
+  if(loggedIn) {
+    console.log('Logged In')
+  } else {
+    console.log('Incorrect credentials')
+  }
+}
+function signUp() {
+  const username = prompt('Enter username')
+  for(const user in users2) {
+    if(users2[user].username === username) {
+      console.log('You already have an account')
+      return
+    }
+  }
+  const password = prompt('Enter password')
+  const email = prompt('Enter email')
+  let id = []
+  const chars = "abcdefghiklmnopqrstuvwxyz"
+  for(let i = 0; i < 6; i++) {
+    id.push(chars[Math.floor(Math.random() * chars.length)])
+  }
+  id = id.join('')
+  const now = new Date()
+  users2.push({
+    _id: id,
+    username: username,
+    email: email,
+    password: password,
+    createdAt: `${now.getDate()}/${now.getMonth()}/${now.getFullYear()} ${now.getHours()}:${now.getMinutes()}`,
+    isLoggedIn: false
+  })
+}
+signUp()
