67fe0edc9cf2045db789fdca0d6806fc60bfd1ed
CaolanCode <cmacmahon3@gmail.com>
2023-02-11 14:37:18 +0000

    commit exercise 1

---
+++ day-10/app.js
@@ -0,0 +1,26 @@
+// Exercise 1
+const a = [4, 5, 8, 9]
+const b = [3, 4, 5, 7]
+const countries = ['Finland', 'Sweden', 'Norway']
+
+const count = new Set()
+for(let i = 0; i < 10; i++) {
+  count.add(i+1)
+}
+console.log(count)
+
+count.delete(4)
+console.log(count)
+
+count.clear()
+console.log(count)
+
+const counties = ['Galway', 'Mayo', 'Offaly', 'Dublin', 'Tipperary']
+const countySet = new Set(counties)
+console.log(countySet)
+
+const countryMap = new Map()
+countries.forEach(country => {
+  countryMap.set(country, country.length)
+})
+console.log(countryMap)
